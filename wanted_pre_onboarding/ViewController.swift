9fcf4ba902144bb8735454c2429ed1b254a48003
Kim-Junhwan <tazanboy1@naver.com>
2022-06-16 17:02:33 +0900

    Initial Commit

---
+++ wanted_pre_onboarding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wanted_pre_onboarding
+//
+//  Created by JunHwan Kim on 2022/06/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
